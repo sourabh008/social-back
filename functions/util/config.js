052fb197d57da6c5379a3c4f5690a4e035a3923a
sourabh kamboj <you@example.com>
2019-11-11 11:17:07 +0530

    first

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyAvOIgWKEPRvIcp0hgYqT-YQisnayyV4cw",
+  authDomain: "socialmediaapp-d1306.firebaseapp.com",
+  databaseURL: "https://socialmediaapp-d1306.firebaseio.com",
+  projectId: "socialmediaapp-d1306",
+  storageBucket: "socialmediaapp-d1306.appspot.com",
+  messagingSenderId: "269263380162",
+  appId: "1:269263380162:web:590228137596a84b"
+  };
